f47bc6abdd262b3872608cf8c1a668147d252593
Ganesh Annan <gannan@digitalbazaar.com>
2021-03-08 01:26:09 -0500

    Update reporting.

---
+++ bin/update-report.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const fs = require('fs').promises;
+const path = require('path');
+
+const dir = path.join(__dirname, '..', 'docs', 'reports');
+
+async function start() {
+  const files = await fs.readdir(dir);
+  const htmlFiles = files.filter(file => file.endsWith('html'))
+    .sort((a, b) => a - b);
+
+  const indexFilePath = path.join(dir, 'index.md');
+  try {
+    await fs.unlink(indexFilePath);
+  } catch(e) {
+    if(!e.message.includes('ENOENT')) {
+      throw e;
+    }
+  }
+
+  const heading = '# Reports';
+  await fs.writeFile(indexFilePath, `${heading}\n\n`, 'ascii');
+
+  for(let i = htmlFiles.length - 1; i >= 0; i--) {
+    const filename = htmlFiles[i];
+    let date = filename.split(/-(.+)/)[1];
+    date = date.substring(0, date.length - 5);
+
+    const reportHeading = i === htmlFiles.length - 1 ?
+      `## Report ${date} (Latest)` : `## Report ${date}`;
+    const fileDescription = `Click [here](./${filename})`;
+
+    await fs.appendFile(indexFilePath, `${reportHeading}\n\n`, 'ascii');
+    await fs.appendFile(indexFilePath, `${fileDescription}\n\n`, 'ascii');
+  }
+}
+
+(async () => {
+  await start();
+})();
